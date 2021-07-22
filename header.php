87880730133e6737250c98c782191d25a3d27801
roberto.valdes <roberto.valdes@estudiantes.upr.edu.cu>
2021-07-21 20:51:54 -0400

    agregando vista profesional

--- header.php
+++ header.php
@@ -38,11 +38,11 @@
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'web-andres' ); ?></a>
-	<header id="masthead" class="site-header">
-		<div id="sideNavigation" class="sidenav">
+	<header id="masthead" class="site-header <?php if($estilo=="profesional") echo "dark_background"; ?>">
+		<div id="sideNavigation" class="sidenav <?php if($estilo=="profesional") echo "dark_background sidenav_dark"; ?>">
 			<div class="menuHeader">
 				<div>
-					<img src="<?php echo get_field("logo_menu_lateral","option") ?>"/>
+					<img src="<?php if($estilo=="amateur") echo get_field("logo_menu_lateral","option"); else echo get_field("logo_menu_lateral_dark","option"); ?>"/>
 				</div>
 				<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
 			</div>
@@ -60,7 +60,7 @@
 			</div>		
 		</div>
 		<?php if(is_front_page()){ ?>
-		<div id="sideNavigation2" class="sidenav2">
+		<div id="sideNavigation2" class="sidenav2 <?php if($estilo=="profesional") echo "dark_background sidenav_dark"; ?>">
 			<div class="menuHeader">
 				<div>
 				<a href="javascript:void(0)" class="closebtn" onclick="returnNav()"><</a>
@@ -84,7 +84,7 @@
 					src=
 					"<?php if($estilo=="profesional")echo get_field("logo_profesional","option"); else echo get_field("logo_amateur","option");?>"/>
 		</div>
-		<div class="menuContainer">
+		<div class="menuContainer <?php if($estilo=="profesional") echo "menuContainerDark";?>">
 			<?php if(get_field("repeater_menu","option")){
 				foreach(get_field("repeater_menu","option") as $menu){ ?>
 			 <a href="<?php echo $menu["item_menu"]["url"] ?>"><?php echo $menu["item_menu"]["title"] ?></a>	
@@ -93,10 +93,18 @@
 		</div>
   		<a class="openNavButton" href="#" onclick="openNav()">
     	<svg width="30" height="30" id="icoOpen">
+		<?php if($estilo=="amateur") {?> 
         	<path d="M0,5 30,5" stroke="#000" stroke-width="5"/>
         	<path d="M0,14 30,14" stroke="#000" stroke-width="5"/>
         	<path d="M0,23 30,23" stroke="#000" stroke-width="5"/>
+		<?php }else{
+		 ?>
+			<path d="M0,5 30,5" stroke="white" stroke-width="5"/>
+        	<path d="M0,14 30,14" stroke="white" stroke-width="5"/>
+        	<path d="M0,23 30,23" stroke="white" stroke-width="5"/>
+		 <?php } ?>
     	</svg>
+
   		</a>
 	</nav>
 	
@@ -104,9 +112,9 @@
 		?>
 		<div class="topnav container mt-3 menuInferior">
 			<div class="logoContainer">
-				<img src="<?php echo get_field("logo_menu_lateral","option") ?>"/>
+			<img src="<?php if($estilo=="amateur") echo get_field("logo_menu_lateral","option"); else echo get_field("logo_menu_lateral_dark","option"); ?>"/>
 			</div>
-			<div class="menuContainer">
+			<div class="menuContainer <?php if($estilo=="profesional") echo "menuContainerDark";?>">
 			<?php foreach($cats as $cat){ ?>
 			 	<a href="<?php echo esc_url(get_category_link(get_cat_ID($cat->name))) ?>"><?php echo $cat->name ?></a>	
 			 <?php } ?>	
