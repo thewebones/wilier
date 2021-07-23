059b6925f07c3e50a7d9b57c6ba6065ed1494709
roberto.valdes <roberto.valdes@estudiantes.upr.edu.cu>
2021-07-23 13:34:10 -0400

    ultimando detalles

--- template-parts/blocks/bloque-eventos/index.php
+++ template-parts/blocks/bloque-eventos/index.php
@@ -6,13 +6,16 @@
     $the_query=new WP_Query($args);
     $estilo=get_field("estilo");
 ?>
-<div class="generalContainer" style="<?php if($estilo=="Profesional") echo "background:black" ?>">
+<div class="generalContainer " style="<?php if($estilo=="Profesional") echo "background:black" ?>">
+<div class="container">
+<h1 style="<?php if($estilo=="Profesional") echo "color:white"; ?>"><?php echo get_field("titulo") ?></h1>
+</div>
 <?php if($the_query->have_posts())
     while($the_query->have_posts()){
         $the_query->the_post();
 ?>
 <div class="eventoContainer container mb-5">
-    <div class="imageInfoContainer responsive" >
+    <div class="imageInfoContainer responsive <?php if($estilo=="Profesional")echo "quitarSombra" ?>" >
     <svg width="100%" height="100%" id="icoOpen">
         <polygon class="poligon" fill="<?php if($estilo=="Profesional") echo "black"; else echo "white"?>" stroke="<?php if($estilo=="Profesional") echo "white" ?>" stroke-width="2"
         points="" />
@@ -21,13 +24,13 @@
     <div class="eventoInfoContainer">
         <p class="evento_categoria responsive"><?php echo get_the_category()[0]->name?></p>
         <p class="evento_modelo responsive"><?php echo the_title() ?></p>
-        <p><?php echo get_the_excerpt() ?> </p>
+        <p class="<?php if($estilo=="Profesional") echo "colorWhite"?>"><?php echo get_the_excerpt() ?> </p>
         <div class="compartir mb-3">
-            <span>Comparti con:</span>
-            <h3><?php echo get_post_meta( get_the_ID(), 'compartido', true ) ?></h3>
+            <span class="<?php if($estilo=="Profesional") echo "colorWhite"?>">Comparti con:</span>
+            <h3 class="<?php if($estilo=="Profesional") echo "colorWhite"?>"><?php echo get_post_meta( get_the_ID(), 'compartido', true ) ?></h3>
         </div>
-        <p class="evento_premio">Premio: <?php echo get_post_meta( get_the_ID(), 'premio', true )?></p>
-        <a href="#" class="btn btn-slider">
+        <p class="evento_premio <?php if($estilo=="Profesional") echo "colorWhite"?>"><?php echo get_post_meta( get_the_ID(), 'premio', true )?></p>
+        <a href="#" class="btn btn-slider <?php if($estilo=="Profesional") echo "btnWhite";?>">
             Consultar 
             <span>
                 <?php if($estilo=="Profesional"){ ?>
@@ -42,7 +45,7 @@
     <div class="imagenEventoContainer" >
         <?php the_post_thumbnail(); ?>
     </div>
-    <div class="fechaEventoContainer" style="background: url('<?php echo get_site_url(); ?>/wp-content/themes/wilier/img/imagefecha.png')">
+    <div class="fechaEventoContainer <?php if($estilo=="Profesional")echo "quitarSombra" ?>" style="background: url('<?php echo get_site_url(); ?>/wp-content/themes/wilier/img/imagefecha.png')">
         <?php
         $fecha=get_post_meta( get_the_ID(), 'fecha_evento', true );
         $date=new DateTime($fecha);
@@ -50,7 +53,7 @@
         <span class="evento_hora"><?php echo $date->format("g:i a"); ?></span>
         <span class="evento_fecha"><?php echo $date->format("F j,Y");?> </span>
     </div>
-    <div class="evento_name_responsive" style="background: url('<?php echo get_site_url(); ?>/wp-content/themes/wilier/img/imagenombre.png')">
+    <div class="evento_name_responsive <?php if($estilo=="Profesional")echo "quitarSombra" ?>" style="background: url('<?php echo get_site_url(); ?>/wp-content/themes/wilier/img/imagenombre.png')">
         <?php
         $fecha=get_post_meta( get_the_ID(), 'fecha_evento', true );
         $date=new DateTime($fecha);
